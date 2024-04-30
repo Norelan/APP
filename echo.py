af1785beef2588ddcda9777d056060ccda839503
Ilya M <il.makaren@yandex.ru>
2024-04-30 15:54:28 +0700

    add secret information

--- echo.py
+++ echo.py
@@ -12,8 +12,6 @@ API_URL = 'https://api.telegram.org/bot'
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-
-
 async def process_start_command(message: Message):
     await message.answer('Привет!\n Меня зовут Wish-бот!\n Пока что я не умею составлять вишлисты, но могу отправить вам фото лисички.\n Напиши мне что-нибудь')
 
@@ -27,8 +25,6 @@ async def process_help_command(message: Message):
 async def send_photo_echo(message: Message):
     print(message)
     await message.reply_photo(message.photo[0].file_id)
-
-
 
 async def send_echo(message: Message):
 
@@ -47,8 +43,6 @@ dp.message.register(process_start_command, Command(commands='start'))
 dp.message.register(process_help_command, Command(commands='help'))
 dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
 dp.message.register(send_echo)
-
-   
 
 if __name__ == '__main__':
     dp.run_polling(bot)
